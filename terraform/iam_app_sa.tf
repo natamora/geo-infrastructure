98cb3dc465c9ad2257e53d5c51d96f008a09638e
Natamora <natamora96@gmail.com>
2026-08-26 05:13:30 +0200

    fix: try to fix build permission error - add secret access role to trigger

--- terraform/iam_app_sa.tf
+++ terraform/iam_app_sa.tf
@@ -7,8 +7,4 @@ resource "google_secret_manager_secret_iam_member" "app_secret_access" {
   secret_id = google_secret_manager_secret.db_password.id
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:${google_service_account.app_runtime_sa.email}"
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
