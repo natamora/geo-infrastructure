5a376dd52dc053bccaa511744742cb427bb05def
Natamora <natamora96@gmail.com>
2026-08-26 04:54:13 +0200

    fix: add missing depends_on for attaching role to unexisting sa account

--- terraform/iam_app_sa.tf
+++ terraform/iam_app_sa.tf
@@ -7,4 +7,8 @@ resource "google_secret_manager_secret_iam_member" "app_secret_access" {
   secret_id = google_secret_manager_secret.db_password.id
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:${google_service_account.app_runtime_sa.email}"
+
+  depends_on = [
+    google_service_account.app_runtime_sa
+  ]
 }
